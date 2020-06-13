7bf0fedef6959cd32071ec7565b8888cf772b3be
Trisha Patel <trisha@curbie.ca>
2020-06-12 21:27:16 -0600

    Create empty file to list jobs

---
+++ src/components/listJobs/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class ListJobs extends React.Component {
+  componentDidMount() {
+    this.fetchIndeedJobs();
+  }
+  fetchIndeedJobs = () => {};
+
+  render() {
+    return <div></div>;
+  }
+}
